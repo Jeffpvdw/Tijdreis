421e961e84bb44930e2e1818eccde569136eca9c
Jelle Kuipers <rj.kuipers@student.alfa-college.nl>
2022-04-04 09:16:36 +0200

    Added Migrations
    Added reservations, Statistics, Participants, Tours and Themes Migrations.

---
+++ database/migrations/2022_03_30_120917_create_participants_table (1).php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParticipantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participants', function (Blueprint $table) {
+            $table->id();
+            $table->integer('reservation_id');
+            $table->string('firstname', 64);
+            $table->string('lastname', 64);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participants');
+    }
+}
