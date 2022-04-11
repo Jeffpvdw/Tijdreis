e7437c1ab77a79c599a2b9e291fc633e76f538b0
Jelle Kuipers <rj.kuipers@student.alfa-college.nl>
2022-04-11 14:27:29 +0200

    fix?

--- app/Models/Tour.php
+++ app/Models/Tour.php
@@ -4,10 +4,7 @@ namespace App\Models;
 
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
 use Illuminate\Database\Eloquent\Model;
-<<<<<<< Updated upstream
 use App\Models\Theme;
-=======
->>>>>>> Stashed changes
 
 class Tour extends Model
 {
@@ -39,13 +36,10 @@ class Tour extends Model
     |--------------------------------------------------------------------------
     */
 
-<<<<<<< Updated upstream
     public function themes(){
         return $this->belongsTo(Theme::class);
     }
 
-=======
->>>>>>> Stashed changes
     /*
     |--------------------------------------------------------------------------
     | SCOPES
