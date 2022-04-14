02aee5480bc26b951f73663a0d0adff732e9f8c0
ronald <r.vandevelde01@student.alfa-college.nl>
2022-04-14 14:07:27 +0200

    streepjes zijn nu arrays

--- resources/views/gegevens.blade.php
+++ resources/views/gegevens.blade.php
@@ -73,7 +73,7 @@
               <div>
                 <label for="first-name" class="block text-sm font-medium text-gray-700">Voornaam</label>
                 <div class="mt-1">
-                  <input type="text" name="first-name_" id="first-name_" autocomplete="given-name"
+                  <input type="text" name="firstName[]" id="firstName[]" autocomplete="given-name"
                     class="py-3 px-4 block w-full shadow-md focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
                     placeholder="Jan" required>
                 </div>
@@ -81,7 +81,7 @@
               <div class="mt-5 sm:mt-0">
                 <label for="proposition" class="block text-sm font-medium text-gray-700">Tussenvoegsel</label>
                 <div class="mt-1">
-                  <input type="text" name="proposition_" id="proposition_" autocomplete="proposition"
+                  <input type="text" name="proposition[]" id="proposition[]" autocomplete="proposition"
                     class="py-3 px-4 block w-full shadow-md focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
                     placeholder="Van">
                 </div>
@@ -89,7 +89,7 @@
               <div class="mt-5 sm:mt-0">
                 <label for="last-name" class="block text-sm font-medium text-gray-700">Achternaam</label>
                 <div class="mt-1">
-                  <input type="text" name="last-name_" id="last-name_" autocomplete="family-name"
+                  <input type="text" name="lastName[]" id="lastName[]" autocomplete="family-name"
                     class="py-3 px-4 block w-full shadow-md focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md"
                     placeholder="Timmer" required>
                 </div>
@@ -98,13 +98,14 @@
             <div class="mt-5">
               <label for="birthday[]" class="block text-sm font-medium text-gray-700">Geboortedatum</label>
               <div>
-                <input type="month" value="2022-04" name="date_" class="py-3 px-4 block w-full shadow-md focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
+                <input type="month" value="2022-04" name="date[]" id="date[]" class="py-3 px-4 block w-full shadow-md focus:ring-indigo-500 focus:border-indigo-500 border-gray-300 rounded-md">
               </div>
             </div>
             <div class="mt-5 divide-y divide-dashed">
               <label for="price" class="block text-sm font-medium text-gray-700">Prijs</label>
-              <div id="price" class="font-semibold text-2xl mt-[8px] mb-8">
-                €10,-
+              <div  class="font-semibold text-2xl mt-[8px] mb-8">
+                <p id="price[]"> €10,-</p>
+                  <input type="hidden" name="prijs[]" value="10">
               </div>
             </div>
           </div>
