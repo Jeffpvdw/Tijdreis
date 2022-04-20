654328a9b825292ebf00f2de1ae5767f0396e74c
jim vogelzang <jimvogelzang2003@gmail.com>
2022-04-20 14:10:18 +0200

    fixed mollie for real

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Cashier\Billable;
 use Laravel\Sanctum\HasApiTokens;
+use Laravel\Cashier\Cashier;
+
 
 
 class User extends Authenticatable
