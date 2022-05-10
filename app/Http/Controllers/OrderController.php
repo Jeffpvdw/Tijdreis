36e7b3dc1a0bdbe01980632d4ce1f0d57626e394
Eden <Voortmanraymond@gmail.com>
2022-05-10 13:07:00 +0200

    slection fixed

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,13 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Tour;
+use App\Models\Theme;
+use Illuminate\Support\Facades\DB;
 use Carbon\Carbon;
+use \stdClass;
 
 class OrderController extends Controller
 {
     public function get()
     {
-        $tours = Tour::where('dateTime', '>',  Carbon::now())->get();
+        $tours = Tour::where('dateTime', '>',  Carbon::now())->with('themes')->get();
+
+        // dd($tours);
         return view('gegevens', ['tours' => $tours]);
     }
 }
