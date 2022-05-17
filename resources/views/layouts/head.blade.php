7ac9e32728e77b1c7c9ffb52190d357493b2c535
JLJCremer <jlj.cremer@student.alfa-college.nl>
2022-05-17 13:57:21 +0200

    icon voor website toe gevoegd

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Tijdreis-Hardenberg</title>
+    <link rel="icon" type="image/x-icon" href="/images/icon.svg">
   <script src="https://cdn.tailwindcss.com"></script>
   <script src="https://kit.fontawesome.com/dead9ccdc0.js" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
