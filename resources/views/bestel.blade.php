22584487f97f20712103ac30361d1d461bbf0d39
Daniël Jansen <dr.jansen01@student.alfa-college.nl>
2022-06-10 10:52:41 +0200

    small code change

--- resources/views/bestel.blade.php
+++ resources/views/bestel.blade.php
@@ -28,9 +28,9 @@
             <div class="mt-8">
               <a 
               {{-- href="/gegevens"  --}}
-              type="submit" class="cursor-not-allowed text-center mt-8 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 
-                flex items-center justify-center text-base font-medium text-white bg-[#808080] opacity-50 
-                focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              type="submit" class="text-center mt-8 w-full bg-indigo-600 border border-transparent rounded-md py-3 px-8 flex items-center justify-center 
+                text-base font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500
+                bg-[#808080] opacity-50 cursor-not-allowed">
                 U kunt boeken op januari 2023</a>
           </form>
           <!-- Product details -->
