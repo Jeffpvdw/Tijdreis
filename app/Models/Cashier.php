f9a0cbabc408f761a73fb6ac795c6a7c1cac0b2b
jim vogelzang <jimvogelzang2003@gmail.com>
2022-06-24 10:27:23 +0200

    added tables to model

--- app/Models/Cashier.php
+++ app/Models/Cashier.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Cashier extends Model
 {
     use HasFactory;
+
+$table->string('mollie_customer_id')->nullable();
+$table->string('mollie_mandate_id')->nullable();
+$table->decimal('tax_percentage', 6, 4)->default(0); // optional
+$table->dateTime('trial_ends_at')->nullable(); // optional
+$table->text('extra_billing_information')->nullable(); // optional
 }
