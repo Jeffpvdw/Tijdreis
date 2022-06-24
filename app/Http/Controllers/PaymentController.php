3f0c4f83e749f50f578b6074beaa0469560fc8b3
jim vogelzang <jimvogelzang2003@gmail.com>
2022-06-24 11:37:04 +0200

    moi

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -15,7 +15,7 @@ class PaymentController
         $user = Participant::where(['reservation_id' => $reservationId])->get();
         $reservation = Reservation::find($reservationId);
 
-        dd($user);
+        dd($reservation);
 
 
         $item = new \Laravel\Cashier\Charge\ChargeItemBuilder($user);
