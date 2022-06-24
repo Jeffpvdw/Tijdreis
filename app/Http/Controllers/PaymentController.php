c6b79c1a99d951645d740756443adbddb3c55a52
jim vogelzang <jimvogelzang2003@gmail.com>
2022-06-24 11:06:24 +0200

    mollie changed

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -13,16 +13,16 @@ class PaymentController
     public function testPayment(int $reservationId)
     {
         $participants = Participant::where(['reservation_id' => $reservationId])->get();
-        $reservation = Reservation::find($reservationId);
+        $user = Reservation::find($reservationId);
 
-      
 
-        $item = new \Laravel\Cashier\Charge\ChargeItemBuilder($participants);
+
+        $item = new \Laravel\Cashier\Charge\ChargeItemBuilder($user);
         $item->unitPrice(money(20000, 'EUR')); //1 EUR
         $item->description('energie-10-06-2022');
         $chargeItem = $item->make();
 
-        $item2 = new \Laravel\Cashier\Charge\ChargeItemBuilder($participants);
+        $item2 = new \Laravel\Cashier\Charge\ChargeItemBuilder($user);
         $item2->unitPrice(money(0, 'EUR'));
         $item2->description('Test Item 4');
         $chargeItem2 = $item2->make();
