f94f1025051b6a0c28145086abec92f402ebdefa
Daniël Jansen <dr.jansen01@student.alfa-college.nl>
2022-06-27 14:28:01 +0200

    removed script for tailwind
    instead of using script from the website, use internal file for tailwind

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Tijdreis-Hardenberg</title>
     <link rel="icon" type="image/x-icon" href="/images/icon.svg">
-  <script src="https://cdn.tailwindcss.com"></script>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <script src="https://kit.fontawesome.com/dead9ccdc0.js" crossorigin="anonymous"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
